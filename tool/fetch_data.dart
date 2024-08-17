0a2da43c9fb0fe4e38136725c55b134c6816c411
Arjan Mels <github@mels.email>
2024-08-17 14:04:03 +0200

    Updated fetch_data to include timezone information

--- tool/fetch_data.dart
+++ tool/fetch_data.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+
 import 'package:common_locale_data/src/supported_locales.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as p;
@@ -12,6 +13,15 @@ void main() async {
     'localenames': {'languages', 'territories'},
     'misc': {'characters', 'listPatterns'},
   };
+  final miscSets = {
+    'bcp47/bcp47': {'timezone'},
+    'core/supplemental': {
+      'aliases',
+      'metaZones',
+      'primaryZones',
+      'windowsZones'
+    }
+  };
 
   var dataDirectory = Directory('tool/data');
   if (dataDirectory.existsSync()) {
@@ -20,6 +30,18 @@ void main() async {
   var client = http.Client();
   var pool = Pool(10);
 
+  for (var set in miscSets.keys) {
+    for (var file in miscSets[set]!) {
+      unawaited(pool.withResource(() async {
+        var url =
+            'https://raw.githubusercontent.com/unicode-org/cldr-json/master/cldr-json/cldr-$set/$file.json';
+        var directory = Directory(p.join(dataDirectory.path, '$set'));
+        var fileName = '$file.json';
+
+        await download(directory, url, client, fileName);
+      }));
+    }
+  }
   for (var set in sets.keys) {
     for (var file in sets[set]!) {
       for (var locale in supportedLocales) {
@@ -27,22 +49,27 @@ void main() async {
           var url =
               'https://raw.githubusercontent.com/unicode-org/cldr-json/master/cldr-json/cldr-$set-modern/main/$locale/$file.json';
           var directory = Directory(p.join(dataDirectory.path, '$set/$file'));
-          if (!directory.existsSync()) {
-            directory.createSync(recursive: true);
-          }
-          var request = http.Request('get', Uri.parse(url));
-          var response = await client.send(request);
-          if (response.statusCode >= 400) {
-            throw Exception(
-                '${response.statusCode} ${response.reasonPhrase} $url');
-          }
+          var fileName = '$locale.json';
 
-          await response.stream
-              .pipe(File(p.join(directory.path, '$locale.json')).openWrite());
+          await download(directory, url, client, fileName);
         }));
       }
     }
   }
 
   await pool.close();
+}
+
+Future<void> download(
+    Directory directory, String url, http.Client client, String file) async {
+  if (!directory.existsSync()) {
+    directory.createSync(recursive: true);
+  }
+  var request = http.Request('get', Uri.parse(url));
+  var response = await client.send(request);
+  if (response.statusCode >= 400) {
+    throw Exception('${response.statusCode} ${response.reasonPhrase} $url');
+  }
+
+  await response.stream.pipe(File(p.join(directory.path, file)).openWrite());
 }
