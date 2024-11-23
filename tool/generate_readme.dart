4b0b076f9ac72c9a78698de748a1cd95c98e9ba2
Xavier Hainaux <xavier.hainaux@gmail.com>
2024-11-23 18:38:46 +0100

    Remove download date from readme

--- tool/generate_readme.dart
+++ tool/generate_readme.dart
@@ -82,9 +82,7 @@ String generateReadme(File source) {
           .join(Platform.isWindows ? '  \r\n' : '  \n'));
 
   var version = getDataVersions();
-
-  readme =
-      readme.replaceAll('##DOWNLOAD_DATE##', version.date.toUtc().toString());
+  
   readme = readme.replaceAll('##CLDR_VERSION##', version.cldr);
   readme = readme.replaceAll('##CLDR_VARIANT##', version.cldrVariant);
   readme = readme.replaceAll('##UNICODE_VERSION##', version.unicode);
